cd44efe2030f1646d8c2ae8ec13e3eb6d3e7a85c
Chaskar Prasad Bhausaheb <72083319+Prasadchaskar@users.noreply.github.com>
2021-11-15 18:26:57 +0530

    Update inference.py

--- inference.py
+++ inference.py
@@ -1,9 +1,8 @@
 import pickle
-import numpy as np
 import pandas as pd
 
-model = pickle.load(open('HealthScore\healthscr.pkl', 'rb'))
-scaler = pickle.load(open('HealthScore\scalar.pkl', 'rb'))
+model = pickle.load(open('healthscr.pkl', 'rb'))
+scaler = pickle.load(open('scalar.pkl', 'rb'))
 class_names = [0,1,2,3]
 
 def predict(df):
@@ -12,23 +11,3 @@ def predict(df):
     predictions = model.predict(df)
     output = [class_names[class_predicted] for class_predicted in predictions]
     return output
-
-temp = 99.1
-pul = 90
-resp = 16
-bpsys = 129
-bpdi=75
-pop = 99
-
-
-df = pd.DataFrame({ 
-    'TEMPF':[temp],
-    'PULSE':[pul], 
-    'RESPR':[resp], 
-    'BPSYS':[bpsys], 
-    'BPDIAS':[bpdi],
-    'BPDIAS':[bpdi],
-    'POPCT':[pop]
-
-})
-print(predict(df))
